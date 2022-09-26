2798aa36ca7ed06b13a85c8af730e9d87e941d31
aman gupta <gaman8690@gmail.com>
2022-09-26 18:34:32 +0530

    my mern app awesome

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+})
+.then(console.log("Connected to mongoDB"))
+.catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null,"images");
+    },filename:(req,file,cb) => {
+        cb(null, "hello.webp");
+    },
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("file has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen(port, ()=>{
+    console.log("Backend is running");
+})
